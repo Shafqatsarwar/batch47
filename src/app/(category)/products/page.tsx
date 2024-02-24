5b939d4ff381141078b68fe3a34e374555ad44c4
Shafqatsarwar <139677807+Shafqatsarwar@users.noreply.github.com>
2024-02-24 01:12:06 +0000

    header

--- src/app/(category)/products/page.tsx
+++ src/app/(category)/products/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Image from "next/image";
+import Header from "@/components/Header";
 
 function Products(){
     return(
-
+        
 <main className="main-container ">All Products
+<Header/>
    <div className="position:fixed;z-index:9999;top:16px;left:16px;right:16px;bottom:16px;pointer-events:none">
     </div>
      <div className="Allproducts-container grid grid-cols-3"> 
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/a6a38f6a1f31dafe5f3294a4384f865b7d25a344-370x394.png" width="250" height="270">
-</img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/a6a38f6a1f31dafe5f3294a4384f865b7d25a344-370x394.png" width="250" height="270">
+</Image >
     <p className="Allproduct-name"> Brushed Raglan Sweatshirt</p>
     <p className="Allproduct-tags">Sweater</p>
     <p className="Allproduct-price">$195</p>
@@ -19,8 +21,8 @@ function Products(){
     </div>
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/ffc858fc182553bee2aaff34fe728bf07d15f2b5-278x296.png" width="250" height="270">
-</img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/ffc858fc182553bee2aaff34fe728bf07d15f2b5-278x296.png" width="250" height="270">
+</Image>
         <p className="Allproduct-name">Cameryn Sash Tie Dress</p>
         <p className="Allproduct-tags"> Dress</p>
         <p className="Allproduct-price">$545</p>
@@ -29,22 +31,22 @@ function Products(){
 
         
         <div className="Allproduct-card">
-        <img src="https://cdn.sanity.io/images/dow10h3v/production/4e2ed6a9eaa6e1413843e53f3113ccfd2104c301-278x296.png" width="250" height="270"></img>
+        <Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/4e2ed6a9eaa6e1413843e53f3113ccfd2104c301-278x296.png" width="250" height="270"></Image>
         <p className="Allproduct-name">Flex Push Button Bomber</p>
         <p className="Allproduct-tags">Jackets</p>
         <p className="Allproduct-price">$225</p>
         </div>
 
         <div className="Allproduct-card">
-        <img src="https://cdn.sanity.io/images/dow10h3v/production/2228e40aa700a68031eff4016f9e625ade813a89-278x296.png" width="250" height="270"></img>
+        <Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/2228e40aa700a68031eff4016f9e625ade813a89-278x296.png" width="250" height="270"></Image>
         <p className="Allproduct-name">Raglan Sweatshirt</p>
         <p className="Allproduct-tags">Sweater</p>
         <p className="Allproduct-price">$195</p>
         </div>
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/bf4d73c91b7c7c2875fbfbb8e1a9c8113c82e90a-370x394.png" width="250" height="270">
-</img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/bf4d73c91b7c7c2875fbfbb8e1a9c8113c82e90a-370x394.png" width="250" height="270">
+</Image>
         <p className="Allproduct-name">Flex Sweatshirt</p>
         <p className="Allproduct-tags">Sweater</p>
         <p className="Allproduct-price">$175</p>
@@ -52,8 +54,8 @@ function Products(){
         </div>
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/ed4960c6cf46ac416ae2fa6c0a3accbaa551457e-278x296.png" width="250" height="270">    
-</img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/ed4960c6cf46ac416ae2fa6c0a3accbaa551457e-278x296.png" width="250" height="270">    
+</Image>
        <p className="Allproduct-name">Flex Sweatpants</p>
         <p className="Allproduct-tags">Pants</p>
         <p className="Allproduct-price">$175</p> 
@@ -61,8 +63,8 @@ function Products(){
         </div>
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/c7b4f3b93e9536ce55353fdd05b7a81a2b57be02-278x296.png" width="250" height="270">
-</img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/c7b4f3b93e9536ce55353fdd05b7a81a2b57be02-278x296.png" width="250" height="270">
+</Image>
         <p className="Allproduct-name">Pink Fleece Sweatpants</p>
         <p className="Allproduct-tags">Pants</p>
         <p className="Allproduct-price">$195</p>
@@ -70,8 +72,8 @@ function Products(){
         </div>
 
 <div className="Allproduct-card">
-<img src="https://funsies.pk/cdn/shop/products/WhatsAppImage2022-06-11at7.29.20AM_2.jpg?v=1654958522" width="250" height="270"> 
-</img>
+<Image alt="products" src="https://funsies.pk/cdn/shop/products/WhatsAppImage2022-06-11at7.29.20AM_2.jpg?v=1654958522" width="250" height="270"> 
+</Image>
 <p className="Allproduct-name">Little Boss Trouser Grey</p>
 <p className="Allproduct-tags">Trouser</p>
 <p className="Allproduct-price">$225</p>
@@ -79,8 +81,8 @@ function Products(){
 
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/7739d01f547a127eca9fdfdb267935974e8d2b83-278x296.png" width="250" height="270">
-</img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/7739d01f547a127eca9fdfdb267935974e8d2b83-278x296.png" width="250" height="270">
+</Image>
         <p className="Allproduct-name">Lite Sweatpants</p>
         <p className="Allproduct-tags">Pants</p>
         <p className="Allproduct-price">$150</p>
@@ -88,8 +90,8 @@ function Products(){
         </div>
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/bfd5b4b468e1a6c84c25589fae1a103cac342102-278x296.png" width="250" height="270">
-</img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/bfd5b4b468e1a6c84c25589fae1a103cac342102-278x296.png" width="250" height="270">
+</Image>
         <p className="Allproduct-name">Imperial Alpaca Hoodie</p>
         <p className="Allproduct-tags">Jackets</p>
         <p className="Allproduct-price">$525</p>
@@ -97,8 +99,8 @@ function Products(){
         </div>
 <div>
 <div className="Allproduct-card">
-<img src="https://cdn.sanity.io/images/dow10h3v/production/d8cbcd5d002fade4d83dc66f910f34ae84d61975-278x296.png" width="250" height="270">
- </img>
+<Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/d8cbcd5d002fade4d83dc66f910f34ae84d61975-278x296.png" width="250" height="270">
+ </Image>
         <p className="Allproduct-name">Muscle Tank</p>
         <p className="Allproduct-tags">T Shirts</p>
         <p className="Allproduct-price">$75</p>
@@ -106,8 +108,8 @@ function Products(){
         </div>
 <div>
   <div className="Allproduct-card">
-  <img src="https://cdn.sanity.io/images/dow10h3v/production/994bf21135a265d4bbeff47cd8fddae6082a0b26-370x394.png" width="250" height="270">
-  </img>
+  <Image alt="products" src="https://cdn.sanity.io/images/dow10h3v/production/994bf21135a265d4bbeff47cd8fddae6082a0b26-370x394.png" width="250" height="270">
+  </Image>
         <p className="Allproduct-name">Brushed Bomber</p>
         <p className="Allproduct-tags">Jackets</p>
         <p className="Allproduct-price"> $225</p>
@@ -115,78 +117,62 @@ function Products(){
         </div>
         </div>
 
-<footer>
-<div className="footer">
-<img alt="logo" srcSet="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo.3267fed8.png&amp;w=256&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo.3267fed8.png&amp;w=384&amp;q=75 2x" 
-src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo.3267fed8.png&amp;w=384&amp;q=75" width="180" height="30" decoding="async" data-nimg="1" loading="lazy"
-className="color:transparent">
-</img>
-<p>Small, artisan label that offers a thoughtfully curated collection of high quality everyday essentials made.</p>
+        <footer className="footer">
+ <div className="card flex flex-row">
+ <div className="logo flex-1">
+    <Image
+    src="https://full-stack-ecommerce-clothing-web.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo.3267fed8.png&w=256&q=75" 
+    alt="logo"
+    width="200" height="50">
+    </Image>
+  <p>Small, artisan label that offers a thoughtfully curated collection of high quality everyday essentials made.</p>
+  </div>
 
-</div>
+<div className="icon-container flex-1">
+  <div className="flex">
+  <svg className=" w-6 h-6 mr-2" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M24,4.3086 C23.117,4.7006 22.168,4.9646 21.172,5.0836 C22.188,4.4746 22.969,3.5096 23.337,2.3596 C22.386,2.9246 21.332,3.3336 20.21,3.5556 C19.312,2.5976 18.032,1.9996 16.616,1.9996 C13.897,1.9996 11.692,4.2046 11.692,6.9236 C11.692,7.3096 11.736,7.6856 11.82,8.0456 C7.728,7.8406 4.099,5.8806 1.671,2.9006 C1.247,3.6286 1.004,4.4736 1.004,5.3766 C1.004,7.0846 1.873,8.5926 3.195,9.4756 C2.388,9.4486 1.628,9.2276 0.964,8.8596 L0.964,8.9206 C0.964,11.3066 2.661,13.2966 4.914,13.7486 C4.501,13.8626 4.065,13.9216 3.617,13.9216 C3.299,13.9216 2.991,13.8906 2.69,13.8336 C3.317,15.7896 5.135,17.2136 7.29,17.2536 C5.604,18.5736 3.481,19.3606 1.175,19.3606 C0.777,19.3606 0.385,19.3376 0,19.2926 C2.179,20.6886 4.767,21.5046 7.548,21.5046 C16.605,21.5046 21.557,14.0016 21.557,7.4946 C21.557,7.2816 21.552,7.0696 21.543,6.8586 C22.505,6.1636 23.34,5.2966 24,4.3086">
+    </path>
+    </svg>
+   
+  <svg className=" w-6 h-6 mr-2"stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M9.94474914,22 L9.94474914,13.1657526 L7,13.1657526 L7,9.48481614 L9.94474914,9.48481614 L9.94474914,6.54006699 C9.94474914,3.49740494 11.8713513,2 14.5856738,2 C15.8857805,2 17.0033128,2.09717672 17.3287076,2.13987558 L17.3287076,5.32020466 L15.4462767,5.32094085 C13.9702212,5.32094085 13.6256856,6.02252733 13.6256856,7.05171716 L13.6256856,9.48481614 L17.306622,9.48481614 L16.5704347,13.1657526 L13.6256856,13.1657526 L13.6845806,22">
+    </path>
+    </svg>
 
-<div className="icon-container"><div>
-<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
- <path fill-rule="evenodd" d="M24,4.3086 C23.117,4.7006 22.168,4.9646 21.172,5.0836 C22.188,4.4746 22.969,3.5096 23.337,2.3596 C22.386,2.9246 21.332,3.3336 20.21,3.5556 C19.312,2.5976 18.032,1.9996 16.616,1.9996 C13.897,1.9996 11.692,4.2046 11.692,6.9236 C11.692,7.3096 11.736,7.6856 11.82,8.0456 C7.728,7.8406 4.099,5.8806 1.671,2.9006 C1.247,3.6286 1.004,4.4736 1.004,5.3766 C1.004,7.0846 1.873,8.5926 3.195,9.4756 C2.388,9.4486 1.628,9.2276 0.964,8.8596 L0.964,8.9206 C0.964,11.3066 2.661,13.2966 4.914,13.7486 C4.501,13.8626 4.065,13.9216 3.617,13.9216 C3.299,13.9216 2.991,13.8906 2.69,13.8336 C3.317,15.7896 5.135,17.2136 7.29,17.2536 C5.604,18.5736 3.481,19.3606 1.175,19.3606 C0.777,19.3606 0.385,19.3376 0,19.2926 C2.179,20.6886 4.767,21.5046 7.548,21.5046 C16.605,21.5046 21.557,14.0016 21.557,7.4946 C21.557,7.2816 21.552,7.0696 21.543,6.8586 C22.505,6.1636 23.34,5.2966 24,4.3086">
-</path>
-</svg>
-</div>
-    <div>
-<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" d="M9.94474914,22 L9.94474914,13.1657526 L7,13.1657526 L7,9.48481614 L9.94474914,9.48481614 L9.94474914,6.54006699 C9.94474914,3.49740494 11.8713513,2 14.5856738,2 C15.8857805,2 17.0033128,2.09717672 17.3287076,2.13987558 L17.3287076,5.32020466 L15.4462767,5.32094085 C13.9702212,5.32094085 13.6256856,6.02252733 13.6256856,7.05171716 L13.6256856,9.48481614 L17.306622,9.48481614 L16.5704347,13.1657526 L13.6256856,13.1657526 L13.6845806,22">
-</path>
-</svg>
-</div>
-    <div>
-<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" d="M22.0367422,22 L17.8848745,22 L17.8848745,15.5036305 C17.8848745,13.9543347 17.85863,11.9615082 15.7275829,11.9615082 C13.5676669,11.9615082 13.237862,13.6498994 13.237862,15.3925291 L13.237862,22 L9.0903683,22 L9.0903683,8.64071385 L13.0707725,8.64071385 L13.0707725,10.4673257 L13.1276354,10.4673257 C13.6813927,9.41667396 15.0356049,8.3091593 17.0555507,8.3091593 C21.2599073,8.3091593 22.0367422,11.0753215 22.0367422,14.6734319 L22.0367422,22 Z M4.40923804,6.81585163 C3.07514653,6.81585163 2,5.73720584 2,4.40748841 C2,3.07864579 3.07514653,2 4.40923804,2 C5.73720584,2 6.81585163,3.07864579 6.81585163,4.40748841 C6.81585163,5.73720584 5.73720584,6.81585163 4.40923804,6.81585163 L4.40923804,6.81585163 Z M6.48604672,22 L2.32980492,22 L2.32980492,8.64071385 L6.48604672,8.64071385 L6.48604672,22 Z">
-</path>
-</svg>
-</div>
-
-        <div className="footer-links">
+  <svg className=" w-6 h-6 mr-2"stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M22.0367422,22 L17.8848745,22 L17.8848745,15.5036305 C17.8848745,13.9543347 17.85863,11.9615082 15.7275829,11.9615082 C13.5676669,11.9615082 13.237862,13.6498994 13.237862,15.3925291 L13.237862,22 L9.0903683,22 L9.0903683,8.64071385 L13.0707725,8.64071385 L13.0707725,10.4673257 L13.1276354,10.4673257 C13.6813927,9.41667396 15.0356049,8.3091593 17.0555507,8.3091593 C21.2599073,8.3091593 22.0367422,11.0753215 22.0367422,14.6734319 L22.0367422,22 Z M4.40923804,6.81585163 C3.07514653,6.81585163 2,5.73720584 2,4.40748841 C2,3.07864579 3.07514653,2 4.40923804,2 C5.73720584,2 6.81585163,3.07864579 6.81585163,4.40748841 C6.81585163,5.73720584 5.73720584,6.81585163 4.40923804,6.81585163 L4.40923804,6.81585163 Z M6.48604672,22 L2.32980492,22 L2.32980492,8.64071385 L6.48604672,8.64071385 L6.48604672,22 Z">
+    </path>
+    </svg>
+    </div>
+    </div>
+<div className="footer-links flex-1">
+        
         <h3>Company</h3>
-        <ul>
         <li>About</li>
         <li>Terms of Use</li>
         <li>Privacy Policy</li>
         <li>How it Works</li>
         <li>Contact Us</li>
-        </ul>
         </div>
-
-        <div className="footer-links">
+<div className="footer-links flex-1"> 
         <h3>Support</h3>
-        <ul>
         <li>Support Carrer</li>
         <li>24h Service</li>
         <li>Quick Chat</li>
-        </ul>
         </div>
-
-        <div className="footer-links">
+<div className="footer-links flex-1"> 
         <h3>Contact</h3>
-        <ul>
         <li>Whatsapp</li>
-        <li>Support 24h</li>
-        </ul>
+        <li>Support 24-Hours</li>
         </div>
         </div>
-
-        <div className="copyright">
-        <p>Copyright © 2022 Dine Market</p>
-        <p>Design by.<span>PIAIC</span>
+        <div className="copyright w-full">
+        <p className="text-center text-blue-900 font-bold">
+        Copyright © 2024 Dine Market
         </p>
-        <p>Code by.<span>BATCH-47</span>
-        </p>
-        </div>
-            
-        <div>
-        <p aria-live="assertive" id="__next-route-announcer__" role="alert" 
-        className="border: 0px; clip: rect(0px, 0px, 0px, 0px); height: 1px; margin: -1px; overflow: hidden; padding: 0px; position: absolute; width: 1px; white-space: nowrap; overflow-wrap: normal;">
-        Dine Market </p>
          </div>
-         </footer>
+        </footer>
             </main>
 );
 };
